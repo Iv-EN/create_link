a1c1fc2d3ba5351d6f738fa31f2ffcdd6adbfd53
Iv-EN <engenivanov@yandex.ru>
2024-03-13 20:10:39 +0300

    Fixed PEP8 errors

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,20 +1,15 @@
-import traceback
-try:
-    from dotenv import load_dotenv
-    from flask import Flask
-    from flask_migrate import Migrate
-    from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-    from settings import Config
+from settings import Config
 
-    load_dotenv()
+load_dotenv()
 
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-    from . import api_views, error_handlers, models, views  # nopep8
-except Exception as e:
-    print('При запуске приложения произошла ошибка:')
-    traceback.print_exc()
+from . import api_views, error_handlers, models, views  # nopep8
