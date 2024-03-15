cea31093b4663d0ff31317628dd19bfb5af3d131
Iv-EN <engenivanov@yandex.ru>
2024-03-15 20:01:08 +0300

    "Expand" condition in index_view

--- yacut/views.py
+++ yacut/views.py
@@ -10,16 +10,17 @@ from .utils import add_url_map, get_urls_for_form
 def index_view():
     """Главная страница генерации коротких ссылок."""
     form = YacutForm()
-    if form.validate_on_submit():
-        original, short_link, error_message = get_urls_for_form(form)
-        if error_message is not None:
-            flash(error_message)
-            return render_template('index.html', form=form)
-        if not add_url_map(original, short_link):
-            flash('Не удалось создать ссылку.')
-            return render_template('index.html', form=form)
-        flash('Ваша короткая ссылка готова:')
-        flash(short_link)
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form)
+    original, short_link, error_message = get_urls_for_form(form)
+    if error_message is not None:
+        flash(error_message)
+        return render_template('index.html', form=form)
+    if not add_url_map(original, short_link):
+        flash('Не удалось создать ссылку.')
+        return render_template('index.html', form=form)
+    flash('Ваша короткая ссылка готова:')
+    flash(short_link)
     return render_template('index.html', form=form)
 
 
